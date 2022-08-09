33c071bba1d5b0c740d46b779c7135f94fb305bc
dannypellatt <danielpellatt92@gmail.com>
2022-08-09 14:15:17 -0400

    tasks 1-3 done

--- app.js
+++ app.js
@@ -3,5 +3,53 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    for (let i = 0; i < count; i++) {
+        if (i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(username = "Danny", age = 31) {
+    let aboveSixteen = "Congrats ${userName, you can drive!";
+    let belowSixteen = "Sorry ${userName, but you need to wait until you're 16.";
+    if (age < 16) {
+        console.log(belowSixteen);
+    }
+    else {
+        console.log(aboveSixteen);
+    }
+}
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+function quadrant(x, y) {
+    if (x > 0) {
+        if (y > 0) {
+            console.log("quadrant 1");
+        }
+        else {
+            console.log("quadrant 4");
+        }
+    }
+    else {
+        if (y > 0) {
+            console.log("quadrant 2");
+        }
+        else {
+            console.log("quadrant 3");
+        }
+    }
+}
+
+// Exercise 4 Section
+console.log("EXERCISE 2:\n==========\n");
+
+function triangle(side1, side2, side3) {
+
+}
